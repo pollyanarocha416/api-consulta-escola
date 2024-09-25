92f91fe255447a115c1f9c873478599a0417b8e2
pollyana.rocha <pollyana.rocha@desktop.net.br>
2024-09-25 12:20:54 -0300

    adicao do modulo matricula

--- escola/models.py
+++ escola/models.py
@@ -23,13 +23,13 @@ class Curso(models.Model):
     
     def __str__(self) -> str:
         return self.codigo
-
 class Matricula(models.Model):
-    nome = models.CharField(max_length = 100)
-    email = models.EmailField(blank=False, max_length = 30)
-    cpf = models.CharField(max_length=11)
-    data_nascimento = models.DateField()
-    celular = models.CharField(max_length= 14)
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
     
-    def __str__(self) -> str:
-        return self.nome
+    estudante=models.ForeignKey(Estudante,on_delete=models.CASCADE)
+    curso=models.ForeignKey(Curso,on_delete=models.CASCADE)
+    periodo=models.CharField(max_length=1, choices=PERIODO, blank=False, null= False, default='M')
--- escola/urls.py
+++ escola/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet,basename='Estudantes')
 router.register('cursos', CursoViewSet,basename='Cursos')
+router.register('matricula', MatriculaViewSet,basename='Matricula')
 
 
 urlpatterns = [
